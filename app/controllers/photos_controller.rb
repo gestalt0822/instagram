42b3b7ca3aa6e61d7a57247eb78bc53ae315902a
青木大介 <AokiDaisuke@aokidaisuke-no-MacBook-Pro.local>
2016-12-05 15:24:52 +0900

    new,createアクションを実装

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,19 @@
 class PhotosController < ApplicationController
   def index
+    @photos = Photo.all
   end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    Photo.create(photos_params)
+    redirect_to photos_path
+  end
+
+  private
+    def photos_params
+      params.require(:photo).permit(:name, :title, :photo, :content)
+    end
 end
