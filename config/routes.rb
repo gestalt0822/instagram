857e166b8d88de3174195c1a4942f76d85882df3
青木大介 <AokiDaisuke@aokidaisuke-no-MacBook-Pro.local>
2016-12-06 15:37:40 +0900

    uidカラムにランダムな値を代入させ、通常ログイン可能に

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'top/index'
 
   devise_for :users, controllers: {
+    registrations: "users/registrations",
     omniauth_callbacks: "users/omniauth_callbacks"
   }
   resources :photos, only: [:index, :new, :create, :edit, :update, :destroy, :show]
