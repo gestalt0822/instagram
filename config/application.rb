f5d6b429ae76f7996c859301aaa8814c108a344b
青木大介 <AokiDaisuke@aokidaisuke-no-MacBook-Pro.local>
2016-12-07 10:57:09 +0900

    エラーメッセージ,一部日本語化,CSS実装

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,8 @@ module Instagram
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
-
+    #http://blog.mknkisk.com/mongoid-translation-missing/
+    #config.i18n.fallbacks = {ja: :en}
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
