3e26d55fb2cbdbd1b3cc8a4e175fa7076e22125f
青木大介 <AokiDaisuke@aokidaisuke-no-MacBook-Pro.local>
2016-12-07 14:18:25 +0900

    user.rbとphoto.rbのアソシエーションを実装

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -11,6 +11,7 @@ class PhotosController < ApplicationController
 
   def create
     @photo = Photo.new(photos_params)
+    @photo.user_id = current_user.id
     if @photo.save
       redirect_to photos_path, notice: "投稿しました！"
     else
